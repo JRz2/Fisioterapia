80533ba0f85a7a996917a67af45f3ca0e2a785b9
john <182749208c@gmail.com>
2024-11-20 22:08:51 -0400

    fix_useredit

--- app/Livewire/UserDatatable.php
+++ app/Livewire/UserDatatable.php
@@ -6,13 +6,19 @@ use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use App\Models\User;
 use Livewire\Attributes\On;
+use PhpParser\Node\Expr\FuncCall;
 
 class UserDatatable extends DataTableComponent
 {
     protected $model = User::class;
     public $usertabla;
     public $userId;
+    public $name;
+    public $email;
+    public $password;
+    public $password_confirmation;
     protected $listeners = ['destroy'];
+    public $openedit;
 
     public function configure(): void
     {
@@ -66,6 +72,44 @@ class UserDatatable extends DataTableComponent
 
     public function createUpdate($data){
         $this->dispatch('confirmUpdate', [$data]); 
+    }
+
+    public function UserEdit($user){
+        $userId = $user['id'];
+        $user = User::find($userId);
+        $this->resetValidation();
+        $this->openedit = true;
+        $this->userId = $user->id;
+        $this->name = $user->name;
+        $this->email = $user->email;
+    }
+
+    public function update(){
+        $this->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required|string|min:8|confirmed',
+        ], [
+            'name' => 'Nombre requerido',
+            'email' => 'Email requerido',
+            'password.required' => 'La contraseña es requerida',
+            'password.confirmed' => 'Las contraseñas no coinciden'
+        ]);
+
+        $user = User::find($this->userId);
+        $user->update([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => bcrypt($this->password),
+            ]);
+
+        $this->dispatch('swal:success', [
+            'title' => 'Usuario',
+            'text' => 'Actualizado Correctamente',
+        ]);
+        $this->openedit = false;
+
+        $this->reset(['name', 'email', 'password', 'password_confirmation']);
     }
 
     public function confirm($dataUser)
